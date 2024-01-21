ef5e13568069aacc71bb09fe9ba40619548d2ad4
iYasha <33287747+iYasha@users.noreply.github.com>
2024-01-21 18:34:21 +0200

    fix: missed some part of doc about replacing env variables

--- substitute_environment_variables.sh
+++ substitute_environment_variables.sh
@@ -3,10 +3,9 @@
 ROOT_DIR=/app
 
 # Replace env vars in files served by NGINX
-cd $ROOT_DIR && npm run build
-#for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
-#do
-#  sed -i 's|VITE_API_URL_PLACEHOLDER|'${VITE_API_URL}'|g' $file
-#  sed -i 's|VITE_AUTH_TOKEN_PLACEHOLDER|'${VITE_AUTH_TOKEN}'|g' $file
-#done
+for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
+do
+  sed -i 's|VITE_API_URL_PLACEHOLDER|'${VITE_API_URL}'|g' $file
+  sed -i 's|VITE_AUTH_TOKEN_PLACEHOLDER|'${VITE_AUTH_TOKEN}'|g' $file
+done
 exec "$@"
